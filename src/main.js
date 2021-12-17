ae12033fb5d9d78a72bb368318ba3a68d810195f
Dooho Lee <jeus_1@naver.com>
2021-12-17 09:59:03 +0900

    [FIX] google analytics for vue

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+    config: {
+        id: "G-23KGQVB87M" // Google Analytics
+    }
+})
+
 createApp(App).use(router).mount('#app')
