8d319f355de1fba249e8e041e1164a31ee349e36
Dooho Lee <jeus_1@naver.com>
2021-12-17 13:04:32 +0900

    [ADD] prerender for SEO

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
+const path = require('path')
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
+
+const productionPlugins = [
+    new PrerenderSpaPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: [
+            "/",
+            "/algo"
+        ],
+        renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+            renderAfterElementExists: '#app'
+        }),
+    }),
+]
+
 module.exports = {
-    publicPath: "/",
-    outputDir: "dist"
+    // publicPath: "/",
+    // outputDir: "dist"
+    lintOnSave: false,
+    configureWebpack: (config) => {
+        if (process.env.NODE_ENV === 'production') {
+            config.plugins.push(...productionPlugins)
+        }
+    }
 }
