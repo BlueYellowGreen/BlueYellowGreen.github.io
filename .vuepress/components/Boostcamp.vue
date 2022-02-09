3999612e5ed240298769549286a4f6ddd5a1b531
Dooho Lee <jeus_1@naver.com>
2022-02-09 09:49:20 +0900

    [ADD] 02/08 table tags

--- .vuepress/components/Boostcamp.vue
+++ .vuepress/components/Boostcamp.vue
@@ -33,7 +33,7 @@ export default {
   data() {
     return {
       til: [
-        [ "0208", [""] ],
+        [ "0208", ["Convolution", "1x1 Convolution", "Semantic Segmentation", "Object Detectoin", "RNN", "LSTM", "GRU"] ],
         [ "0207", ["Deep Learning", "Optimization", "Gradient Descent Methods", "Regularization"] ],
         [ "0204", ["Text", "Color", "Facet", "Seaborn"] ],
         [ "0203", ["Matplotlib", "Bar Plot", "Line Plot", "Scatter Plot"] ],
